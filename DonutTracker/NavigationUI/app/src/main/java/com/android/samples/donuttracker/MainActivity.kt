c8439b84eb91fb41a113c53866be5266e1e65d63
Murat Yener <yenerm@google.com>
2021-04-06 18:01:44 -0700

    NavigationUI starter code

--- DonutTracker/NavigationUI/app/src/main/java/com/android/samples/donuttracker/MainActivity.kt
+++ DonutTracker/NavigationUI/app/src/main/java/com/android/samples/donuttracker/MainActivity.kt
@@ -20,17 +20,7 @@ import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.onNavDestinationSelected
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.android.samples.donuttracker.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationView
 
 /**
  * Main activity class. Not much happens here, just some basic UI setup.
@@ -38,42 +28,14 @@ import com.google.android.material.navigation.NavigationView
  */
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var navController: NavController
-    private lateinit var appBarConfiguration: AppBarConfiguration
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
-
-        val navHostFragment = supportFragmentManager.findFragmentById(
-            R.id.nav_host_fragment
-        ) as NavHostFragment
-        navController = navHostFragment.navController
-
-        appBarConfiguration= AppBarConfiguration(navController.graph)
-
-        val toolbar = binding.toolbar
-        setSupportActionBar(toolbar)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        setupBottomNavMenu(navController)
-        setupNavigationMenu(navController)
+        setSupportActionBar(binding.toolbar)
 
         Notifier.init(this)
-    }
-
-    private fun setupNavigationMenu(navController: NavController){
-        val sideNavView = findViewById<NavigationView>(R.id.nav_view)
-        sideNavView?.setupWithNavController(navController)
-    }
-    private fun setupBottomNavMenu(navController: NavController){
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
-        bottomNav?.setupWithNavController(navController)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -86,8 +48,9 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return item.onNavDestinationSelected(
-            findNavController(R.id.nav_host_fragment)
-        ) || super.onOptionsItemSelected(item)
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
